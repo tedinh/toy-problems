172d2964149a670d3a54605d9b84e8f5f8b7acc1
Terry Dinh <terrydinh@Terrys-MacBook-Pro.local>
2017-11-27 16:34:11 -0600

    return negative

---
+++ 8kyu/Return_Negative.js
@@ -0,0 +1,13 @@
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+function makeNegative(num) {
+    if(num === 0) {
+        return 0
+    }
+    else if ( num > 0) {
+        return num * -1;
+    }
+    else {
+        return num;
+    }
+  }
